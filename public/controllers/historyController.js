723a3c3a371dfbca9097bb3d039c7c208d20b5d2
Karen Ma <karenma333@gmail.com>
2017-02-17 14:41:45 -0800

    lol it works now for no reason

--- public/controllers/historyController.js
+++ public/controllers/historyController.js
@@ -1,8 +1,47 @@
-/*angularApp.controller('historyController', function ($scope) {
-
-});*/
-//TEMP CODE
 angularApp.controller('historyController', function($scope, $http, $rootScope, $location, $window, $animate) {
+  if (!isLoggedIn()) {
+    $rootScope.hideNavBar = true;
+  }
+  $scope.submitting = $rootScope.FB;
+  $scope.fbLogin = function () {
+    $scope.error = null;
+    FB.login(function(response) {
+      if (response.status === 'connected') {
+        if (response.authResponse.grantedScopes.split(',').length !== 2) {
+          $scope.$apply(function () {
+            $scope.error = 'This app needs permissions to your Facebook events to work.';
+            $scope.submitting = false;
+          });
+          return;
+        }
+        var data = {token: response.authResponse.accessToken};
+        $http.post('/api/login', data)
+          .then(function success(response) {
+            if ($location.path() === '/home')
+              $window.location.reload();
+            else
+              $location.path('home');
+          }, function failure(response) {
+            if (response.status === 401) {
+              // User never registered. Take him/her to register page
+              $location.path('register');
+            } else if (response.status === 403) {
+              $rootScope.registered = true;
+              $location.path('/register/confirmation');
+            } else {
+              $scope.error = 'Unknown error occurred. Please try again later.';
+            }
+            $scope.submitting = false;
+          });
+      } else { // User didn't go through with the FB login
+        $scope.$apply(function () {
+          $scope.submitting = false;
+        });
+      }
+    }, {scope: 'user_events', return_scopes: true, auth_type: 'rerequest'});
+    $scope.submitting = true;
+  };
+
   $http.get('/api/events/all')
     .then(function success(response) {
       $scope.events = response.data;
@@ -10,31 +49,67 @@ angularApp.controller('historyController', function($scope, $http, $rootScope, $
       // TODO
     });
 
+  function showSnackBar(message, undoHandler, onTimeout) {
+    var snackbarContainer = document.querySelector('#events-snackbar');
+    var timeout = 2500;
+    var undo = false;
+    let options = {
+      message: message,
+      timeout: timeout
+    };
+    if (undoHandler) {
+      options.actionText = 'Undo';
+      options.actionHandler = function () {
+        if (undo)
+          return;
+
+        undo = true;
+        undoHandler();
+      }
+    }
+    snackbarContainer.MaterialSnackbar.showSnackbar(options);
+    setTimeout(function () {
+      if (undo) {
+        return;
+      }
+      if (onTimeout) {
+        onTimeout();
+      }
+    }, timeout + 500);
+  }
+
+  $scope.dismissEvent = function (event) {
+    var index = $scope.events.indexOf(event);
+    $scope.events.splice(index, 1);
+    showSnackBar(event.title + ' dismissed', function undoHandler() {
+      $scope.events.splice(index, 0, event);
+      $scope.$apply();
+    }, function onTimeout() {
+      // TODO update event on server
+    });
+  };
+
   var bound = false;
   function bindAutoComplete() {
-    autocomplete.addListener('place_changed', function () {
-      let place = autocomplete.getPlace();
+    autoCompleteRide.addListener('place_changed', function () {
+      let place = autoCompleteRide.getPlace();
       if (place.place_id)
         $scope.place = place;
     });
     bound = true;
   }
-  if (autocomplete) {
+  if (autoCompleteRide) {
     bindAutoComplete();
   }
 
   var modal = $('#eventsRideModal');
   var currentEvent = null;
   $scope.getRide = function (event) {
-    if (!bound && !autocomplete) {
-      var snackbarContainer = document.querySelector('#events-snackbar');
-      snackbarContainer.MaterialSnackbar.showSnackbar({
-        message: 'Please wait',
-        timeout: 2500
-      });
+    if (!bound && !autoCompleteRide) {
+      showSnackBar('Please wait');
       return;
     }
-    if (!bound && autocomplete) {
+    if (!bound && autoCompleteRide) {
       bindAutoComplete();
     }
 
@@ -43,12 +118,28 @@ angularApp.controller('historyController', function($scope, $http, $rootScope, $
     currentEvent = event;
   };
 
-  modal.find('form').submit(function (e) {
+  var modalForm = modal.find('form');
+  modal.find('.submit-btn').click(function () {
+    modalForm.submit();
+  });
+  modal.find('.cancel-btn').click(function () {
+    modal.modal('toggle');
+  });
+  modalForm.submit(function (e) {
     e.preventDefault();
     if ($scope.place) {
       console.log('Successfully selected place:', $scope.place, ' for event ', currentEvent);
-      // TODO
       modal.modal('toggle');
+      var event = currentEvent;
+      var index = $scope.events.indexOf(event);
+      $scope.events.splice(index, 1);
+      $scope.$apply();
+      showSnackBar('We will look for rides', function undoHandler() {
+        $scope.events.splice(index, 0, event);
+        $scope.$apply();
+      }, function onTimeout() {
+        // TODO submit request to server to fix a ride
+      });
     }
   });
   modal.on('hidden.bs.modal', function () {
