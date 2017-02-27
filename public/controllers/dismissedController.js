3e8c88e579643c6be373f2f7b5e4df75111babd2
Raghav Mehta <raghav.mehta1996@gmail.com>
2017-02-27 15:41:19 -0800

    Logic for dismissed page implemented

--- public/controllers/dismissedController.js
+++ public/controllers/dismissedController.js
@@ -1,3 +1,34 @@
-angularApp.controller('dismissedController', function ($scope) {
+angularApp.controller('dismissedController', function ($scope, $rootScope, $http) {
+  $http.get('/api/events/dismissed')
+    .then(function success(response) {
+      response.data.forEach(function (event) {
+        event.startTime = new Date(event.startTime);
+      });
+      $scope.events = response.data;
+    }, function failure() {
+      $rootScope.showSnackbar('Unknown error occurred');
+    });
 
+  var now = new Date();
+  $scope.isInFuture = function (event) {
+    return event.startTime.getTime() > now.getTime();
+  };
+
+  $scope.isInPast = function (event) {
+    return event.startTime.getTime() <= now.getTime();
+  };
+
+  $scope.restoreEvent = function (event) {
+    var index = $scope.events.indexOf(event);
+    $scope.events.splice(index, 1);
+    $rootScope.showSnackbar('Restoring Event', function undoHandler() {
+      $scope.events.splice(index, 0, event);
+      $scope.$apply();
+    }, function onTimeout() {
+      $http.put('/api/events/' + event._id + '/restore', null)
+        .then(function () {}, function () {
+          $rootScope.showSnackbar('Unknown error occurred');
+        });
+    });
+  };
 });
--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const events = require('../events.json');
 const User = require('../models/User');
 const Event = require('../models/Event');
+const publicFields = 'name description startTime fbEventId cover';
 
 
 /**
@@ -32,7 +33,45 @@ router.get('/upcoming', (req, res) => {
     Event.find({
         _id: {$in: eventIds},
         startTime: {$gt: now}
-      }, 'name description startTime fbEventId cover',
+      }, publicFields,
+      {sort: {startTime: 1}},
+      (err, events) => {
+        if (err) {
+          return res.sendStatus(500);
+        }
+        res.json(events);
+      });
+  });
+});
+
+
+/**
+ * GET: /api/events/dismissed
+ *
+ * Get the users dismissed events
+ *
+ * EXPECTS: Nothing
+ *
+ * RESPONDS: [{name description startTime fbEventId cover}],
+ *        Code 401 is no user is logged in
+ */
+router.get('/dismissed', (req, res) => {
+  if (!req.userId) {
+    return res.sendStatus(401);
+  }
+
+  User.findById(req.userId, (err, user) => {
+    if (err || !user) {
+      return res.sendStatus(500);
+    }
+    let eventIds = user.events.filter(event => {
+      return event.dismissed;
+    }).map(event => {
+      return event.eventId;
+    });
+    Event.find({
+        _id: {$in: eventIds}
+      }, publicFields,
       {sort: {startTime: 1}},
       (err, events) => {
         if (err) {
@@ -75,6 +114,63 @@ router.put('/:id/dismiss', (req, res) => {
               return res.sendStatus(500);
             }
             res.sendStatus(200);
+          });
+        });
+        return;
+      }
+    }
+    res.sendStatus(404);
+  });
+});
+
+
+/**
+ * PUT: /api/events/:id/dismiss
+ *
+ * Restore a dismissed event for the logged in user
+ *
+ * EXPECTS: Nothing
+ * RESPONDS: Code 200 if all OK,
+ *          Code 401 if no user is logged in,
+ *          Code 404 user is not associated to the event id
+ */
+router.put('/:id/restore', (req, res) => {
+  if (!req.userId) {
+    return res.sendStatus(401);
+  }
+
+  User.findById(req.userId, (err, user) => {
+    if (err || !user) {
+      return res.sendStatus(500);
+    }
+
+    for (let i = 0; i < user.events.length; i++) {
+      if (user.events[i].eventId == req.params.id) {
+        user.events[i].dismissed = false;
+        user.save((err) => {
+          if (err)
+            return res.sendStatus(500);
+          Event.findOne({$or: [{fbEventId: req.params.id}, {_id: req.params.id}]}, (err, event) => {
+            if (err || !event) {
+              return res.sendStatus(500);
+            }
+            let contains = false;
+            for (let i = 0; i < event.participants.length; i++) {
+              if (event.participants[i] == user.id) {
+                contains = true;
+                break;
+              }
+            }
+            if (contains) {
+              return res.sendStatus(200);
+            }
+            event.participants.push(user.id);
+            event.save((err) => {
+              if (err) {
+                return res.sendStatus(500);
+              }
+              res.sendStatus(200);
+            });
           });
         });
         return;
